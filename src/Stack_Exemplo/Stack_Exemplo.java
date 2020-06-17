4e37328d3dc25c100df426a590bd827563b54817
Cláudio Leal <claudiolealti@gmail.com>
2020-06-17 20:11:35 -0300

    Stak

---
+++ src/Stack_Exemplo/Stack_Exemplo.java
@@ -0,0 +1,15 @@
+package Stack_Exemplo;
+
+public class Stack_Exemplo {
+
+	public static void main(String[] args) {
+		int valorInicial = 7;
+		int valorFinal = calcular(valorInicial);
+		System.out.println("Este é o valor final: " + valorFinal + "!");
+	}
+	public static int calcular (int dados) {
+		int valorTemporario = dados;
+		int novoValor = valorTemporario * 2;
+		return novoValor;
+	}
+}
