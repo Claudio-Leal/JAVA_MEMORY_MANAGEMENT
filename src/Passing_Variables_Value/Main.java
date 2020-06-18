ed3dd1c9b9ef20381fd2bf50ad73c57ccbaae54b
Cláudio Leal <claudiolealti@gmail.com>
2020-06-18 19:02:05 -0300

    Passing variables by value - versão 3

--- src/Passing_Variables_Value/Main.java
+++ src/Passing_Variables_Value/Main.java
@@ -1,28 +1,14 @@
-/*
- * Vamos testar o comportamento entre STACK E HEAP. 
- * Registros importantes em pullrequest.
- */
-
 package Passing_Variables_Value;
 
 public class Main {
 
 	public static void main(String[] args) {
 		int localValue = 5;
-		// chamada do método calculate, sendo passado a variável localValue
-		// nos parâmetros. 
-		calculate(localValue);
-		System.out.println(localValue);
+		int newValue = calculate(localValue);
+		System.out.println("Este é o novo valor de localValue: " + newValue + "!");
 	}
-	// Criação de um novo método que será chamado no método main.
-	// public: poderá ser chamado de qualquer local da aplicação.
-	// static: método poderá ser chamado, sem que seja necessário instanciá-lo.
-	// void: retornará vazio.
-	// calculate: nome do método.
-	// argumentos: como parâmetro, está sendo passado a variável calcValue, 
-	// do tipo int. 
-	public static void calculate (int calcValue) {
-		// corpo do método.
-		calcValue = calcValue * 100;
+	public static int calculate (int localValue) {	
+		localValue = localValue * 100;
+		return localValue;
 	}
 }
